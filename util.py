2ff5b8b719c71384af313a7f4098ac931c9383af
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 20:23:20 +0700

    Update util.py

--- util.py
+++ util.py
@@ -1,9 +1,7 @@
 import base64
-
 import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
-
 
 def set_background(image_file):
     """
@@ -28,7 +26,6 @@ def set_background(image_file):
     """
     st.markdown(style, unsafe_allow_html=True)
 
-
 def classify(image, model, class_names):
     """
     This function takes an image, a model, and a list of class names and returns the predicted class and confidence
@@ -42,22 +39,21 @@ def classify(image, model, class_names):
     Returns:
         A tuple of the predicted class name and the confidence score for that prediction.
     """
-    # convert image to (224, 224)
-    image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
+    # Resize the image to (224, 224)
+    image = image.resize((224, 224))
 
-    # convert image to numpy array
+    # Convert image to numpy array
     image_array = np.asarray(image)
 
-    # normalize image
+    # Normalize image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # set model input
+    # Set model input
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     data[0] = normalized_image_array
 
-    # make prediction
+    # Make prediction
     prediction = model.predict(data)
-    # index = np.argmax(prediction)
     index = 0 if prediction[0][0] > 0.95 else 1
     class_name = class_names[index]
     confidence_score = prediction[0][index]
