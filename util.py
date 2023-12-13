153296a1837c1c0b93109c30e8ab80c4bbed8d42
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 20:25:00 +0700

    Update util.py

--- util.py
+++ util.py
@@ -1,13 +1,14 @@
 import base64
-import streamlit as st
-from PIL import ImageOps, Image
+from PIL import Image
 import numpy as np
+import io
 
-def set_background(image_file):
+def set_background(st, image_file):
     """
     This function sets the background of a Streamlit app to an image specified by the given image file.
 
     Parameters:
+        st: Streamlit object
         image_file (str): The path to the image file to be used as the background.
 
     Returns:
@@ -26,33 +27,37 @@ def set_background(image_file):
     """
     st.markdown(style, unsafe_allow_html=True)
 
-def classify(image, model, class_names):
+
+def classify(image_data, model, class_names):
     """
-    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
+    This function takes raw image data, a model, and a list of class names and returns the predicted class and confidence
     score of the image.
 
     Parameters:
-        image (PIL.Image.Image): An image to be classified.
+        image_data (bytes): Raw image data from Streamlit file uploader.
         model (tensorflow.keras.Model): A trained machine learning model for image classification.
         class_names (list): A list of class names corresponding to the classes that the model can predict.
 
     Returns:
         A tuple of the predicted class name and the confidence score for that prediction.
     """
-    # Resize the image to (224, 224)
+    # Convert raw image data to PIL Image
+    image = Image.open(io.BytesIO(image_data)).convert('RGB')
+
+    # Resize image to (224, 224)
     image = image.resize((224, 224))
 
-    # Convert image to numpy array
+    # convert image to numpy array
     image_array = np.asarray(image)
 
-    # Normalize image
+    # normalize image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # Set model input
+    # set model input
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     data[0] = normalized_image_array
 
-    # Make prediction
+    # make prediction
     prediction = model.predict(data)
     index = 0 if prediction[0][0] > 0.95 else 1
     class_name = class_names[index]
