346cd5b24c4322ba89dbb32d08573a4f1f786933
Alsyahrir <153171771+Alsyahrir@users.noreply.github.com>
2023-12-13 20:52:40 +0700

    Update util.py

--- util.py
+++ util.py
@@ -44,22 +44,20 @@ def classify(image, model, class_names):
     Returns:
         A tuple of the predicted class name and the confidence score for that prediction.
     """
-    # convert image to (224, 224)
+    # Convert image to (224, 224)
     image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
 
-    # convert image to numpy array
+    # Convert image to numpy array
     image_array = np.asarray(image)
 
-    # normalize image
+    # Normalize image
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # set model input
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    data[0] = normalized_image_array
+    # Ensure correct input shape
+    data = np.expand_dims(normalized_image_array, axis=0)
 
-    # make prediction
+    # Make prediction
     prediction = model.predict(data)
-    # index = np.argmax(prediction)
     index = 0 if prediction[0][0] > 0.95 else 1
     class_name = class_names[index]
     confidence_score = prediction[0][index]
